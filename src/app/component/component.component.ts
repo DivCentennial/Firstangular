45e8eb94d40d7e11019d7601aff2f3650da302b0
Divyanshoo Sinha <107339622+DivyanshooSinha@users.noreply.github.com>
2025-08-30 21:31:53 +0530

    Save work before Angular 17 upgrade

--- src/app/component/component.component.ts
+++ src/app/component/component.component.ts
@@ -9,4 +9,21 @@ export class ComponentComponent {
   eUrl: string = 'https://www.google.co.in/';
   edis: boolean = false;
   clcolor: string = "c1";
+name: string = "mariapps";
+  str :string = "Hello friends!!";
+  a: number = 10;
+  b: number = 20;
+clicked(){ 
+ this.str = this.str =='Hello friends!!'? 'Button clicked' :'Hello friends!!'
+ }
+
+//  ✅ Equivalent if-else
+// clicked() {
+//   if (this.str == 'Hello friends!!') {
+//     this.str = 'Button clicked';
+//   } else {
+//     this.str = 'Hello friends!!';
+//   }
+// }
+
 }
